5d7a6e31048dffb90d0147f933b3e2f80cdbd69a
Roland Laboulaye <rlaboulaye@gmail.com>
2024-03-19 08:59:28 -0400

    dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::env;
+
+use futures::TryStreamExt;
+use noodles::vcf;
+use tokio::{
+    fs::File,
+    io::{self, BufReader},
+};
+
+pub async fn read(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut reader = File::open(&path)
+        .await
+        .map(BufReader::new)
+        .map(vcf::AsyncReader::new)?;
+
+    let header = reader.read_header().await?;
+
+    let mut records = reader.records(&header);
+
+    while let Some(record) = records.try_next().await? {
+//        println!("{:?}", record);
+        println!("in while");
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -8,7 +32,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = read("/media/storage/1000_genomes/GRCh38/variants/ALL.chr1.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz");
+        assert_eq!(result, Ok(()));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use clonehort::add;
+use clonehort::read;
 
 fn main() {
-    let sum = add(1, 2);
-    println!("Hello, {sum} worlds!");
+    let path = String::from("/media/storage/1000_genomes/GRCh38/variants/ALL.chr1.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz");
+    read(&path);
 }
