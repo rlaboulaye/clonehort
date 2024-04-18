c4e1162e4cee4e60a36b1203110a19776ed767a7
Roland Laboulaye <rlaboulaye@gmail.com>
2024-04-18 11:31:58 -0400

    htslib

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,40 @@
-use std::env;
+use rust_htslib::bcf::{Reader, Read};
+use std::convert::TryFrom;
 
-use futures::TryStreamExt;
-use noodles::vcf;
-use tokio::{
-    fs::File,
-    io::{self, BufReader},
-};
+pub fn read(path: &str) {
+    let mut vcf = Reader::from_path(path).expect("Error opening file.");
+    // iterate through each row of the vcf body.
+    for (i, record_result) in vcf.records().enumerate() {
+        let mut record = record_result.expect("Fail to read record");
+        let mut s = String::new();
+         for allele in record.alleles() {
+             for c in allele {
+                 s.push(char::from(*c))
+             }
+             s.push(' ')
+         }
+        // 0-based position and the list of alleles
+        println!("Locus: {}, Alleles: {}", record.pos(), s);
+        // number of sample in the vcf
+        let sample_count = usize::try_from(record.sample_count()).unwrap();
 
-pub async fn read(path: &str) -> Result<(), Box<dyn std::error::Error>> {
-
-    let mut reader = File::open(&path)
-        .await
-        .map(BufReader::new)
-        .map(vcf::AsyncReader::new)?;
-
-    let header = reader.read_header().await?;
-
-    let mut records = reader.records(&header);
-
-    while let Some(record) = records.try_next().await? {
-//        println!("{:?}", record);
-        println!("in while");
+        // Counting ref, alt and missing alleles for each sample
+        let mut n_ref = vec![0; sample_count];
+        let mut n_alt = vec![0; sample_count];
+        let mut n_missing = vec![0; sample_count];
+        let gts = record.genotypes().expect("Error reading genotypes");
+        for sample_index in 0..sample_count {
+            // for each sample
+            for gta in gts.get(sample_index).iter() {
+                // for each allele
+                match gta.index() {
+                    Some(0) => n_ref[sample_index] += 1,  // reference allele
+                    Some(_) => n_alt[sample_index] += 1,  // alt allele
+                    None => n_missing[sample_index] += 1, // missing allele
+                }
+            }
+        }
     }
-
-    Ok(())
 }
 
 #[cfg(test)]
@@ -32,7 +43,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = read("/media/storage/1000_genomes/GRCh38/variants/ALL.chr1.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz");
-        assert_eq!(result, Ok(()));
+//        read();
+//        assert_eq!(result, Ok(()));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clonehort::read;
 
 fn main() {
-    let path = String::from("/media/storage/1000_genomes/GRCh38/variants/ALL.chr1.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz");
+    let path = String::from("/media/storage/1000_genomes/GRCh38/variants/chr20/mxl.chr20.chunk1.GRCh38.vcf.gz");
+    //let path = String::from("/media/storage/1000_genomes/GRCh38/variants/chr20/mxl.chr20.GRCh38.vcf.gz");
     read(&path);
 }
